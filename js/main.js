1d791fc7bb2a173eb240766bdc7e0584d91ae6a3
Alexander Cheng <videogame5678@gmail.com>
2014-10-13 14:52:58 -0500

    GUI Files
    added GUI files to start.  Set up a login.  Needs CSS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//var url = "http://lyle.smu.edu/~jmmurphy/cse3345/http/menu.json";
+//var request = new XMLHttpRequest();
+//request.open('GET', url, false);
+//request.send();
+
+//if(request.status === 200){
+//    var json = JSON.parse(request.responseText);
+//}
+
