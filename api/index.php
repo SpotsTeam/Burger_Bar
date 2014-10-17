0cf901ae5e6dec05eb717eaae028c9bbce038e2c
Austin Wells <acwells@smu.edu>
2014-10-17 17:18:49 -0500

    changing database connect

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
-$mysqli = new mysqli("162.243.227.48", "root", "compassstudios", "mydb");
+$mysqli = new mysqli("http://162.243.227.48/Burger_Bar/", "root", "compassstudios", "mydb");
 if ($mysqli->connect_errno)
     die("Connection failed: " . $mysqli->connect_error);
 
