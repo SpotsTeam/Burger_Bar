4338d3bee199188840e43d0c4c5a443bc7089dd5
RSWSMUGirl2016 <rsward@smu.edu>
2014-10-17 01:11:04 -0500

    API calls added to js

--- js/main.js
+++ js/main.js
@@ -16,8 +16,80 @@
 //document.getElementById("signInEmail").removeAttribute("required");
 //document.getElementById("signInPass").removeAttribute("required");
 
+var url = "./api/index.php/getMeats";
+var request = new XMLHttpRequest();
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.meats.length; i++){
+		html += '<input type="radio" name="meat" value="'+json.meats[i].name+'" id="'+json.meats[i].name+'">'+json.meats[i].name;
+	}
+	document.getElementById("meats").innerHTML = html;
+}
 
-/*$(document).on('click', '.button', function(){ 
+url = "./api/index.php/getBuns";
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.buns.length; i++){
+		html += '<input type="radio" name="bun" value="'+json.buns[i].name+'" id="'+json.buns[i].name+'">'+json.buns[i].name;
+	}
+	document.getElementById("buns").innerHTML = html;
+}
+
+url = "./api/index.php/getCheeses";
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.cheeses.length; i++){
+		html += '<input type="radio" name="cheese" value="'+json.cheeses[i].name+'" id="'+json.cheeses[i].name+'">'+json.cheeses[i].name;
+	}
+	document.getElementById("cheeses").innerHTML = html;
+}
+
+url = "./api/index.php/getToppings";
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.toppings.length; i++){
+		html += '<input type="checkbox" name="topping" value="'+json.toppings[i].name+'" id="'+json.toppings[i].name+'">'+json.toppings[i].name;
+	}
+	document.getElementById("toppings").innerHTML = html;
+}
+
+url = "./api/index.php/getSauces";
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.sauces.length; i++){
+		html += '<input type="checkbox" name="sauce" value="'+json.sauces[i].name+'" id="'+json.sauces[i].name+'">'+json.sauces[i].name;
+	}
+	document.getElementById("sauces").innerHTML = html;
+}
+
+url = "./api/index.php/getSides";
+request.open('GET', url, false);
+request.send();
+if(request.status === 200){
+    var json = JSON.parse(request.responseText);
+    var html = '';
+	for(var i = 0; i < json.sides.length; i++){
+		html += '<input type="radio" name="side" value="'+json.sides[i].name+'" id="'+json.sides[i].name+'">'+json.sides[i].name;
+	}
+	document.getElementById("sides").innerHTML = html;
+}
+
+$(document).on('click', '.button', function(){ 
    var bttn = $(this).attr('id'); 
    if(bttn === "register"){
 		document.getElementById("signInEmail").removeAttribute("required");
@@ -26,7 +98,7 @@
    } else {
    		return false;
    }
-});*/
+});
 
 var meats = document.getElementsByName("meat");
 function selectMeat(){
